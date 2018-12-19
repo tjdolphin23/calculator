107ad02aaa54ff633152195f4e168334d69f5777
Tom Dolphin <thedolphingroup1@gmail.com>
2018-12-19 13:09:51 -0700

    able to console.log button value

--- main.js
+++ main.js
@@ -1,11 +1,44 @@
 $(document).ready(function(){
 
 
-$("#one").on("click", function(){
-	;
-})
+// $("button").click(function(){
+// 	$(this).attr("value");
+// });
 
+	$("#one").click(function() {
+		console.log($(this).attr("value"));
+	});
+	
+	$("#two").click(function() {
+		console.log($(this).attr("value"));
+	});
 
+	$("#three").click(function() {
+		console.log($(this).attr("value"));
+	});
+	
+	$("#four").click(function() {
+		console.log($(this).attr("value"));
+	});
+	
+	$("#five").click(function() {
+		console.log($(this).attr("value"));
+	});
+	
+	$("#six").click(function() {
+		console.log($(this).attr("value"));
+	});
+	
+	$("#seven").click(function() {
+		console.log($(this).attr("value"));
+	});
 
+	$("#eight").click(function() {
+		console.log($(this).attr("value"));
+	});
+
+	$("#nine").click(function() {
+		console.log($(this).attr("value"));
+	});
 
 });
